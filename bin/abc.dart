861d4f7c08fb6e39d359e88e293b205041d4f7d1
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-03-28 11:24:37 +0530

    Initial commit

---
+++ bin/abc.dart
@@ -0,0 +1,12 @@
+void main(){
+  String x = "Hello";
+  // type allocated according to the initial value => var
+   var  y = 20;
+       // y = "hello";  not possible
+       // y = 40;
+  // type change according to value change => dynamic
+  dynamic z = "hello";
+          z = 30;
+
+
+}
